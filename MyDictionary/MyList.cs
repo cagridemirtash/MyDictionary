32809bbebac848e38f5aae2c8e65e9dcc3541099
cagridemirtash <cagridemirtash@gmail.com>
2021-02-16 02:54:31 +0300

    Add project files.

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<T>
+    {
+        T[] myList;
+        public MyList()
+        {
+            myList = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = myList;
+            myList = new T[myList.Length + 1 ];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                myList[i] = tempArray[i];
+            }
+
+            myList[myList.Length - 1] = item;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Product = new List<string>();
+            Product.Add("Ayakkabi");
+            Product.Add("Canta");
+
+            MyList<string> myProduct = new MyList<string>();
+            myProduct.Add("Ayakkabi");
+            myProduct.Add("T-Shirt");
+            
+            //Liste yazdirma bicimi value=key mantiginda
+            foreach (var item in Product)
+            {
+                Console.WriteLine(value: item);
+            }
+        }
+    }
+}
